9ca841dc6b6f120ae768372cfd78f1954f1c77ee
Zero <114285045+Z3r0Magic@users.noreply.github.com>
2024-05-22 02:28:12 +1000

    Add files via upload

--- SSFfront/end.php
+++ SSFfront/end.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'include/db.php';
-
+$Textid = $_GET['feedback'];
 $idcheck = $_GET['Button'];
 
-$sql = "INSERT INTO rating (client_name, rating, comment) VALUES ('FRONT', '{$idcheck}', 'FRONT')";
+$sql = "INSERT INTO rating (client_name, rating, comment) VALUES ('FRONT', '{$idcheck}', '{$Textid}')";
 if ($conn->query($sql) === TRUE) { 
     echo "Запись добавлена успешно";
 } else { 
